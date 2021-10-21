c674627960ff00279e85c143f3b0012d0d9ba205
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-10-21 17:04:20 -0400

    Add data, function to read the data, and a vingette

---
+++ R/read.R
@@ -0,0 +1,20 @@
+#' Basic function to read in a file
+#'
+#' @param .file Path to a local file.
+#' @param .col_names From vroom
+#' @param .col_select From vroom
+#' @param .name_repair From vroom
+#'
+#' @export
+read <- function(.file,
+                 .col_names = FALSE,
+                 .col_select = everything(),
+                 .name_repair = "unique") {
+  vroom::vroom(
+    file = .file,
+    col_names = .col_names,
+    col_select = .col_select,
+    show_col_types = FALSE,
+    .name_repair = .name_repair
+  )
+}
---
+++ R/testactionspkg-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr everything
+## usethis namespace: end
+NULL
