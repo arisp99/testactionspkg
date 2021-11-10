a4917cd9899522564ea9ee57bc5b1be2485827b5
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-11-09 20:51:43 -0500

    Add document workflow and example fn

---
+++ R/example-fn.R
@@ -0,0 +1,18 @@
+#------------------------------------------------
+#' Arithmetic Mode
+#'
+#' Compute the arithmetic mode of a set of data. The mode is the most commonly
+#' appearing value.
+#'
+#' @param x A vector or list.
+#'
+#' @return The mode of the data.
+#' @export
+#' @examples
+#' mode(c(1, 1, 2))
+#' mode(list(1, 1, 2))
+#' mode(c(1, NA, 1))
+mode <- function(x) {
+  unique <- unique(x)
+  unique[which.max(tabulate(match(x, unique)))]
+}
